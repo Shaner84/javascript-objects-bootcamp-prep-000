50b67718bab83d347a7efd9a89b59bc462c46670
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-08 01:02:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { artistName: 'songTitle' }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, { [Phil Ochs]: songTitle })
+Object.assign({}, playlist, { artistName: "Here's to the State of Mississippi" })
+    return playlist
 }
