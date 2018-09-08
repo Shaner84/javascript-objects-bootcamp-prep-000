cf0f3a450cf76ce098e6a9baf9e6e952e324447c
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-08 00:28:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { artistName: "songTitle" }
 
 function updatePlaylist(Object, String, songTitle){
-  return object.assign({}, playlist, { Slowdive: ['Alison'], }),
+  return object.assign({}, playlist, { Slowdive: ['Alison']}) playlist, { My Bloody Valentine: ['Sometimes'] }),
 }
