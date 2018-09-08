5fe92504dd8d3fd383f08b0d5ee49ef7954340f6
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-08 01:04:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   Object.assign({}, playlist, { "Phil Ochs": songTitle })
     return playlist
 }
+
+function updatePlaylist(object, key, value) {
+  return Object.assign({}, object, { [key]: value })
+}
